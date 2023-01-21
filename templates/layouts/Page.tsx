c03f9e6f1fc4cdb0da6aa4ee217dc671b0288801
mscholz.dev <mscholz.dev@gmail.com>
2023-01-21 21:57:17 +0100

    add user scalable no

--- templates/layouts/Page.tsx
+++ templates/layouts/Page.tsx
@@ -20,7 +20,7 @@ const Page: FC<IPage> = ({
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
         />
         <title>{`EXACT FORM • ${title}`}</title>
         <meta
