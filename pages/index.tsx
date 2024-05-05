b595eb2110254920a73b4d0af7d45a429f620267
mscholz.dev <mscholz.dev@gmail.com>
2024-05-05 03:28:24 +0200

    change home page text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
-import React, {
-  FC,
-  useEffect,
-  useState,
-} from "react";
+import React, { FC, useEffect, useState } from "react";
 import Page from "../templates/layouts/Page";
 import useTranslation from "next-translate/useTranslation";
 import AuthApi from "./api/auth";
@@ -46,42 +42,20 @@ const Index: FC<IIndex> = ({ locale }) => {
       <h1
         style={{
           fontSize: "32px",
-          color: "red",
-          marginBottom: "24px",
+          margin: "24px",
+          lineHeight: "normal",
         }}
       >
-        A L&apos;INTENTION DES UTILISATEURS
+        La page d'accueil n'est pas stylisé, le reste des pages et
+        fonctionnalités sont disponibles,{" "}
+        <u>tester de vous créer un compte !</u>
       </h1>
-      <p style={{ fontSize: "24px" }}>
-        Ce site est actuellement en cours de
-        développement, cela signifie que chaque
-        compte, chaque donnée stockée est
-        suceptible de se faire supprimer.
-        <br />
-        <br />
-        Considérez ce site comme une préprod , ce
-        qui n&apos;est pas le cas par faute de
-        moyen.
-        <br />
-        <br />
-        <span
-          style={{
-            color: "blue",
-            fontWeight: "bold",
-          }}
-        >
-          La fin de la V1 du site est prévu pour
-          début mai.
-        </span>
-      </p>
     </Page>
   );
 };
 
 export default Index;
 
-export const getServerSideProps = async ({
-  locale,
-}: TLocale) => {
+export const getServerSideProps = async ({ locale }: TLocale) => {
   return { props: { locale } };
 };
