82a11bd67dce7cbe730b4070771a4542739c6dd0
mscholz.dev <mscholz.dev@gmail.com>
2024-05-13 15:51:28 +0200

    add log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,34 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { AppProps } from "next/app";
 import "../styles/main.scss";
 import "react-toastify/dist/ReactToastify.css";
 import "regenerator-runtime/runtime";
 
-const App: FC<AppProps> = ({
-  Component,
-  pageProps,
-}) => {
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const logoLog = async () => {
+      const logoStyle = (logo: string) => `
+        background-image: url(${`data:image/svg+xml;base64,${btoa(logo)}`});
+        background-repeat: no-repeat;
+        background-size: 32px;
+        color: white;
+        font-family: sans-serif;
+        font-size: 32px;
+        padding-left: 42px;
+      `;
+
+      const logo1 = await (await fetch(`/mscholzdev/logo-1.svg`)).text();
+      console.info("%cCréé par Morgan Scholz avec ❤️", logoStyle(logo1));
+
+      const logo2 = await (await fetch(`/mscholzdev/logo-2.svg`)).text();
+      console.info("%chttps://mscholz.dev", logoStyle(logo2));
+
+      const logo3 = await (await fetch(`/mscholzdev/logo-3.svg`)).text();
+      console.info("%cmscholz.dev@gmail.com", logoStyle(logo3));
+    };
+    logoLog();
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
